cbcc0d7ab58fe46d41d63ef0795fd696bf4ab461
sreejath <sreejath@hotmail.com>
2015-03-27 18:24:35 -0400

    Implemented Value Exception Also checking in Instructions.txt, just to get it off the list. Nothing related.

--- RPSMine.py
+++ RPSMine.py
@@ -2,11 +2,18 @@ __author__ = 'sreejath'
 import sys
 import math
 import random
+
 """
 Get validated number of games
 
 """
+
+
 def getGames():
+    """
+
+
+    """
     n_games = 3
     selectGames = True
     print("Welcome to Rock, Paper Scissors.")
@@ -14,9 +21,18 @@ def getGames():
 
         try:
             n_games = int(input("Please enter the number of games you would like to play:"))
-        except ValueError:
-            print("Please enter an integer")
+
+            if n_games < 3:
+                raise ValueError(
+                    "To be fair, we need to play at least three games.\n Please enter an odd number that is three or higher.")
+            if 0 == n_games % 2:
+                raise ValueError(
+                    "To determine a clear winner, we must play an odd number of games.\n Please enter an odd number that is three or higher.")
+            selectGames = False
+        except ValueError as v:
+            print(v)
             continue
+
 
 """
 Play game
