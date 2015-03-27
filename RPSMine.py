67ec27dd919ef304cdba2beb7f75a9a565af1726
sreejath <sreejath@hotmail.com>
2015-03-27 18:32:18 -0400

    Completed input validation. About to code game code.

--- RPSMine.py
+++ RPSMine.py
@@ -23,14 +23,19 @@ def getGames():
             n_games = int(input("Please enter the number of games you would like to play:"))
 
             if n_games < 3:
-                raise ValueError(
-                    "To be fair, we need to play at least three games.\n Please enter an odd number that is three or higher.")
+                raise ArithmeticError(
+                    "To be fair, we need to play at least three games.\n"
+                    "Please enter an odd number that is three or higher.")
             if 0 == n_games % 2:
-                raise ValueError(
-                    "To determine a clear winner, we must play an odd number of games.\n Please enter an odd number that is three or higher.")
+                raise ArithmeticError(
+                    "To determine a clear winner, we must play an odd number of games.\n"
+                    "Please enter an odd number that is three or higher.")
             selectGames = False
         except ValueError as v:
-            print(v)
+            print("Please enter an odd number greater than or equal to 3.")
+            continue
+        except ArithmeticError as a:
+            print(a)
             continue
 
 
